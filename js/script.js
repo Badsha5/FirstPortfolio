8913f5e01f9c6235d15c343ae54bedcd9e7228c4
Badsha5 <104724205+Badsha5@users.noreply.github.com>
2022-06-24 12:24:30 -0700

    CREATED INITIALY

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/*   typing animation */
+var typed= new Typed(".typing",{
+    Strings:["Web Designer","Web Developer","Graphic Designer","YouTuber"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+})
+var typed= new Typed(".typing1",{
+    Strings:["Web Designer","Web Developer","Graphic Designer","YouTuber"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+})
